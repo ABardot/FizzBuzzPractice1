ae350fae686d06e08e05fe02509f0ef22cbff196
ABardot <aabardot5@gmail.com>
2018-05-04 16:58:22 -0400

    Fizz Buzz displays without int and just strings

--- FizzBuzzPractice1/FizzBuzzPractice1/Program.cs
+++ FizzBuzzPractice1/FizzBuzzPractice1/Program.cs
@@ -12,24 +12,40 @@ namespace FizzBuzzPractice1
             // 5 - Buzz
             // 15 - FizzBuzz
 
-            for (int i = 0; i < 100; i++) // Divisible by 100
+            //for (int i = 0; i < 100; i++) // Divisible by 100
+            //{
+            //    if (i % 15 == 1) // Started at 1 instead of 0 because 0 will always spell FizzBuzz
+            //    {
+            //        Console.WriteLine("Fizz Buzz");
+            //    }
+            //    else if (i % 5 == 1)
+            //    {
+            //        Console.WriteLine("Buzz");
+            //    }
+            //    else if (i % 3 == 1)
+            //    {
+            //        Console.WriteLine("Fizz");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //} 
+
+            // 3 - Fizz
+            // 7 - Buzz
+            // 21 - FizzBuzz
+
+            for (int i = 1; i < 100; i++)
             {
-                if (i % 15 == 1) // Started at 1 instead of 0 because 0 will always spell FizzBuzz
-                {
-                    Console.WriteLine("FizzBuzz");
-                }
-                else if (i % 5 == 1)
-                {
-                    Console.WriteLine("Buzz");
-                }
-                else if (i % 3 == 1)
-                {
-                    Console.WriteLine("Fizz");
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }
+                var outOut = "";
+
+                if(i % 3 == 1) { outOut += "Fizz"; }
+                if (i % 7 == 1) { outOut += "Buzz"; }
+                if (i % 15 == 1) { outOut += "Fizz Buzz"; }
+
+                Console.WriteLine(outOut);
+
             }
         }
     }
