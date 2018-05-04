b19cabbac28423a7d8ffac802f1b8dc232c86848
ABardot <aabardot5@gmail.com>
2018-05-04 16:38:37 -0400

    Long form of Fizz Buzz

--- FizzBuzzPractice1/FizzBuzzPractice1/Program.cs
+++ FizzBuzzPractice1/FizzBuzzPractice1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace FizzBuzzPractice1
 {
@@ -10,6 +7,30 @@ namespace FizzBuzzPractice1
     {
         static void Main(string[] args)
         {
+
+            // 3 - Fizz
+            // 5 - Buzz
+            // 15 - FizzBuzz
+
+            for (int i = 0; i < 100; i++) // Divisible by 100
+            {
+                if (i % 15 == 1) // Started at 1 instead of 0 because 0 will always spell FizzBuzz
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 1)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 1)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
