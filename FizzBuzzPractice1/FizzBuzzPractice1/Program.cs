be5f8e03c6ca92b03be51d7a39c27d3aaa4802a5
ABardot <aabardot5@gmail.com>
2018-05-04 17:23:50 -0400

    Added classes for TDD testing and NUnit

--- FizzBuzzPractice1/FizzBuzzPractice1/Program.cs
+++ FizzBuzzPractice1/FizzBuzzPractice1/Program.cs
@@ -12,41 +12,41 @@ namespace FizzBuzzPractice1
             // 5 - Buzz
             // 15 - FizzBuzz
 
-            //for (int i = 0; i < 100; i++) // Divisible by 100
-            //{
-            //    if (i % 15 == 1) // Started at 1 instead of 0 because 0 will always spell FizzBuzz
-            //    {
-            //        Console.WriteLine("Fizz Buzz");
-            //    }
-            //    else if (i % 5 == 1)
-            //    {
-            //        Console.WriteLine("Buzz");
-            //    }
-            //    else if (i % 3 == 1)
-            //    {
-            //        Console.WriteLine("Fizz");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine(i);
-            //    }
-            //} 
+            for (int i = 0; i < 100; i++) // Divisible by 100
+            {
+                if (i % 15 == 1) // Started at 1 instead of 0 because 0 will always spell FizzBuzz
+                {
+                    Console.WriteLine("Fizz Buzz");
+                }
+                else if (i % 5 == 1)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 1)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
 
             // 3 - Fizz
             // 7 - Buzz
             // 21 - FizzBuzz
 
-            for (int i = 1; i < 100; i++) // Fizz Buzz without int only strings
-            {
-                var outOut = "";
+            //for (int i = 1; i < 100; i++) // Fizz Buzz without int only strings
+            //{
+            //    var outOut = "";
 
-                if (i % 3 == 1) { outOut += "Fizz"; }
-                if (i % 7 == 1) { outOut += "Buzz"; }
-                if (i % 15 == 1) { outOut += "Fizz Buzz"; }
+            //    if (i % 3 == 1) { outOut += "Fizz"; }
+            //    if (i % 7 == 1) { outOut += "Buzz"; }
+            //    if (i % 21 == 1) { outOut += "Fizz Buzz"; }
 
-                Console.WriteLine(outOut);
+            //    Console.WriteLine(outOut);
 
-            }
+            //}
 
         } // Main end
     }
